f2714fa114cf47bfe2c737edc78ee8597d19bdaf
Gregg <willcoxgregg@gmail.com>
2020-09-12 23:31:03 -0700

    Created Fog (best viewed in Game mode, not Scene view)

---
+++ Assets/Rendering/depthCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class depthCalculator
+{
+
+    public float CalcDepth(Vector3 viewAngle)
+    {
+        return 0.5f;
+    }
+}
